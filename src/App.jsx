c1e53af0d84ab006e2291797d589ccf64abd6287
Edwin García <spark.com.co@gmail.com>
2019-10-10 16:58:34 -0500

    Configuración del entorno de desarrollo usando styled-components

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+const App = () => (
+  <>
+    <GlobalStyles />
+    <h1>App works!</h1>
+  </>
+);
+
+export default App;
